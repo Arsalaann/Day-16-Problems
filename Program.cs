03f6662f25d4feb5a0558d19c12dc001a577b926
Arsalaann <arsalankhanxk@gmail.com>
2023-05-19 21:17:10 +0530

    Binary search

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Day16Problems{
+﻿using System;
+using System.IO;
+namespace Day16Problems{
     class program{
         public static void Main(string [] args){
-            PermutationOfString.PermutationOfStringIterative("abcd");
-            Console.WriteLine("Permutation of String Recursively");
-            PermutationOfString.PermutationOfStringRecursively("\nabcd","");
+            //PermutationOfString.PermutationOfStringIterative("abcd");
+            //Console.WriteLine("Permutation of String Recursively");
+            //PermutationOfString.PermutationOfStringRecursively("\nabcd","");
+            string textFile="dummy.txt";
+            string[] words=File.ReadAllText(textFile).Split(' ');
+            Console.Write("Enter word to search: ");
+            string inp=Console.ReadLine();
+            if(words.Contains(inp)==true)
+            	Console.WriteLine("It Exists");
+            else 
+            	Console.WriteLine("Not exists");
         }
     }
 }
